e09fdfebe8fa38d4ecb8cf6f317c1d5fc201f558
rung <eleven_g11@qq.com>
2018-01-07 14:37:09 +0800

    fetch to json

---
+++ src/fetchJson.js
@@ -0,0 +1,34 @@
+/**
+ * 
+ * 
+ * @param {string} location 请求地址
+ * @param {string} method 请方式
+ * @param {object} mes 参数
+ * @returns {promise}
+ */
+const fetchJson = (location, method, mes) => {
+    if (method.toUpperCase() === 'GET') {
+        Object.keys(mes)
+        let param = '?'
+        for (let key in mes) {
+            param = param + `&${key}=${mes[key]}`
+        }
+        return fetch(location.concat(param.replace('&', ''))).then(response => response.json())
+    }
+    else if (method.toUpperCase() === 'POST') {
+        const headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            ...mes.headers
+        }
+        const body = JSON.stringify(mes.body)
+        const par = { headers, body }
+        console.log(par)
+        return fetch(location, { method, headers, body }).then(response => response.json())
+    }
+    else {
+        throw Error('请求方法错误')
+    }
+}
+
+export default fetchJson;
